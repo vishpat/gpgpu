8a7976b89952720dab613dcbb0186d84b756570b
Vishal Patil <vishpat@gmail.com>
2024-02-04 10:05:34 -0500

    Updated example

--- tensors/src/main.rs
+++ tensors/src/main.rs
@@ -1,4 +1,4 @@
-use candle_core::{Device, Tensor};
+use candle_core::{Device, Tensor, DType};
 use ndarray::array;
 
 #[allow(dead_code)]
@@ -21,8 +21,8 @@ fn example2() -> Result<(), Box<dyn std::error::Error>> {
     let device = Device::new_cuda(0)?;
     let tensor = Tensor::from_slice(&arr_slice, (2, 3), &device)?;
 
-    let tensor_ones = Tensor::ones_like(&tensor)?;
-    let slice = tensor_ones.to_vec2::<f64>()?;
+    let tensor_ones = Tensor::ones(tensor.shape(), DType::F32, &device)?;
+    let slice = tensor_ones.to_vec2::<f32>()?;
     println!("Ones tensor {:?}", slice);
 
     let tensor_rand = Tensor::rand(0f32, 1., tensor.shape(), &device)?;
