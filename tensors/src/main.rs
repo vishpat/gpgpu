a750aa4a5c949b30607326cc9c7532cac0c86335
Vishal Patil <vishpat@gmail.com>
2024-02-04 08:41:42 -0500

    ndarray to Candle tensor

--- tensors/src/main.rs
+++ tensors/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use candle_core::{Device, Tensor};
+use ndarray::array;
+
+fn example1() -> Result<(), Box<dyn std::error::Error>> {
+    let arr = array![[1., 2.], [4., 5.]];
+    println!("ndarray: {:?}", arr);
+
+    let arr_slice = arr.into_shape((2, 2)).to_owned()?.into_raw_vec();
+    println!("Slice: {:?}", arr_slice);
+
+    let device = Device::new_cuda(0)?;
+    let tensor = Tensor::from_slice(&arr_slice, (2, 2), &device);
+    println!("Candle {:?}", tensor);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    example1()?;
+    Ok(())
 }
