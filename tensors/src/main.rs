84748d2d3ea848d8db1dd056ca37fc304a2c852e
Vishal Patil <vishpat@gmail.com>
2024-02-04 09:41:34 -0500

    Get tensors of the same shape

--- tensors/src/main.rs
+++ tensors/src/main.rs
@@ -16,13 +16,19 @@ fn example1() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn example2() -> Result<(), Box<dyn std::error::Error>> {
-    let arr = array![[1., 2.], [4., 5.]];
-    let arr_slice = arr.into_shape((2, 2)).to_owned()?.into_raw_vec();
+    let arr = array![[1., 2., 3.], [4., 5., 6.]];
+    let arr_slice = arr.into_shape((2, 3)).to_owned()?.into_raw_vec();
     let device = Device::new_cuda(0)?;
-    let tensor = Tensor::from_slice(&arr_slice, (2, 2), &device)?;
-    let tensor2 = Tensor::rand(0f32, 1., tensor.shape(), &device)?;
-    let slice = tensor2.to_vec2::<f32>()?;
-    println!("Candle {:?}", slice);
+    let tensor = Tensor::from_slice(&arr_slice, (2, 3), &device)?;
+
+    let tensor_ones = Tensor::ones_like(&tensor)?;
+    let slice = tensor_ones.to_vec2::<f64>()?;
+    println!("Ones tensor {:?}", slice);
+
+    let tensor_rand = Tensor::rand(0f32, 1., tensor.shape(), &device)?;
+    let slice = tensor_rand.to_vec2::<f32>()?;
+    println!("Rand tensor {:?}", slice);
+
     Ok(())
 }
 fn main() -> Result<(), Box<dyn std::error::Error>> {
