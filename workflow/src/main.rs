879ab904ba0fe603a3f7c0e081e7ed2d384275d3
Vishal Patil <vishpat@gmail.com>
2024-02-11 22:32:17 -0500

    Checkpoint

--- workflow/src/main.rs
+++ workflow/src/main.rs
@@ -2,11 +2,6 @@ use candle_core::{Device, Tensor, D};
 use candle_datasets::vision::mnist;
 use candle_nn::ops::log_softmax;
 
-fn log_softmax2(x: &Tensor) -> Result<Tensor, Box<dyn std::error::Error>> {
-    let x = log_softmax(x, D::Minus1)?;
-    Ok(x)
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dataset = mnist::load()?;
     println!("Train images: {:?}", dataset.train_images);
@@ -16,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let device = Device::cuda_if_available(0)?;
     let tensor = Tensor::arange(0f32, 3f32, &device)?;
     println!("Tensor {tensor}");
-    let tensor = log_softmax2(&tensor)?;
+    let tensor = log_softmax(&tensor, D::Minus1)?;
     println!("Log softmax Tensor {tensor}");
     Ok(())
 }
