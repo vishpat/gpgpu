3384c7f3d34999c1f018460b14950d6a3bdf5920
Vishal Patil <vishpat@gmail.com>
2024-02-19 21:34:41 -0500

    Training / Test data

--- linear-regression/src/main.rs
+++ linear-regression/src/main.rs
@@ -13,18 +13,21 @@ const NORTHEAST: f32 = 0.2;
 const SOUTHWEST: f32 = 0.3;
 const SOUTHEAST: f32 = 0.4;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn features_labels() -> Result<(Vec<Vec<f32>>, Vec<f32>), Box<dyn std::error::Error>> {
     let file = File::open("insurance.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
+    let mut features: Vec<Vec<f32>> = vec![];
+    let mut labels: Vec<f32> = vec![];
+
     for result in rdr.records() {
         let record = result?;
-        let age: f32 = record[0].parse()?;
+        let age: f32 = (record[0].parse::<u32>()? as f32) / 100.0;
         let gender = match record[1].parse::<String>()?.as_str() {
             "male" => MALE,
             "female" => FEMALE,
             _ => panic!("Invalid Gender"),
         };
-        let bmi: f32 = record[2].parse()?;
+        let bmi: f32 = record[2].parse::<f32>()? / 100.0;
         let children: f32 = record[3].parse()?;
         let smoker = match record[4].parse::<String>()?.as_str() {
             "yes" => YES,
@@ -40,14 +43,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
         let charges: f32 = record[6].parse()?;
 
-        let features = vec![age, gender, bmi, children, smoker, region];
-        let y = charges;
+        let row = vec![age, gender, bmi, children, smoker, region];
+        features.push(row);
+
+        let label = charges;
+        labels.push(label);
 
         println!(
             "{age} {:?} {bmi} {children} {:?} {:?} {charges}",
             gender, smoker, region
         );
     }
+    Ok((features, labels))
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (features, labels) = features_labels()?;
+    let training_size = (features.len() as f32 * 0.8) as usize;
+
+    let train_features = &features[0..training_size];
+    let train_labels = &labels[0..training_size];
+
+    let test_features = &features[training_size..];
+    let test_labels = &labels[training_size..];
 
     Ok(())
 }
