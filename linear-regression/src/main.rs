abe92d0a4c6e67855649a9c2a5cb8e492a8b04df
Vishal Patil <vishpat@gmail.com>
2024-02-19 20:27:10 -0500

    Able to convert CSV to floating points

--- linear-regression/src/main.rs
+++ linear-regression/src/main.rs
@@ -2,25 +2,16 @@ extern crate csv;
 use core::panic;
 use std::fs::File;
 
-#[derive(Debug)]
-enum Gender {
-    Male = 0,
-    Female = 1,
-}
+const MALE: f32 = 0.0;
+const FEMALE: f32 = 1.0;
 
-#[derive(Debug)]
-enum Region {
-    NorthWest = 0,
-    NorthEast = 1,
-    SouthWest = 2,
-    SouthEast = 3,
-}
+const YES: f32 = 1.0;
+const NO: f32 = 0.0;
 
-#[derive(Debug)]
-enum Smoker {
-    Yes = 0,
-    No = 1,
-}
+const NORTHWEST: f32 = 0.1;
+const NORTHEAST: f32 = 0.2;
+const SOUTHWEST: f32 = 0.3;
+const SOUTHEAST: f32 = 0.4;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("insurance.csv")?;
@@ -29,25 +20,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let record = result?;
         let age: f32 = record[0].parse()?;
         let gender = match record[1].parse::<String>()?.as_str() {
-            "male" => Gender::Male,
-            "female" => Gender::Female,
+            "male" => MALE,
+            "female" => FEMALE,
             _ => panic!("Invalid Gender"),
         };
         let bmi: f32 = record[2].parse()?;
         let children: f32 = record[3].parse()?;
         let smoker = match record[4].parse::<String>()?.as_str() {
-            "yes" => Smoker::Yes,
-            "no" => Smoker::No,
+            "yes" => YES,
+            "no" => NO,
             _ => panic!("Invalid Smoker"),
         };
         let region = match record[5].parse::<String>()?.as_str() {
-            "northwest" => Region::NorthWest,
-            "northeast" => Region::NorthEast,
-            "southwest" => Region::SouthWest,
-            "southeast" => Region::SouthEast,
+            "northwest" => NORTHWEST,
+            "northeast" => NORTHEAST,
+            "southwest" => SOUTHWEST,
+            "southeast" => SOUTHEAST,
             _ => panic!("Invalid Region"),
         };
         let charges: f32 = record[6].parse()?;
+
+        let features = vec![age, gender, bmi, children, smoker, region];
+        let y = charges;
+
         println!(
             "{age} {:?} {bmi} {children} {:?} {:?} {charges}",
             gender, smoker, region
